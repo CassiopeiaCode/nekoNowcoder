a02ec94ebf6afa93e70f99c6e031557afa99e9d9
CassiopeiaCode <i@5i.gs>
2025-07-18 20:30:28 +0800

    Automated deployment of frontend on 2025-07-18 20:30:28

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,6 +1,175 @@
 export const sidebar = [
   {
     "text": "Solutions",
-    "items": []
+    "items": [
+      {
+        "text": "Problem 225893",
+        "link": "/Solution/225893.md"
+      },
+      {
+        "text": "Problem 225765",
+        "link": "/Solution/225765.md"
+      },
+      {
+        "text": "Problem 223734",
+        "link": "/Solution/223734.md"
+      },
+      {
+        "text": "Problem 210275",
+        "link": "/Solution/210275.md"
+      },
+      {
+        "text": "Problem 210274",
+        "link": "/Solution/210274.md"
+      },
+      {
+        "text": "Problem 210259",
+        "link": "/Solution/210259.md"
+      },
+      {
+        "text": "Problem 210256",
+        "link": "/Solution/210256.md"
+      },
+      {
+        "text": "Problem 210247",
+        "link": "/Solution/210247.md"
+      },
+      {
+        "text": "Problem 210246",
+        "link": "/Solution/210246.md"
+      },
+      {
+        "text": "Problem 210245",
+        "link": "/Solution/210245.md"
+      },
+      {
+        "text": "Problem 210244",
+        "link": "/Solution/210244.md"
+      },
+      {
+        "text": "Problem 210242",
+        "link": "/Solution/210242.md"
+      },
+      {
+        "text": "Problem 210241",
+        "link": "/Solution/210241.md"
+      },
+      {
+        "text": "Problem 210240",
+        "link": "/Solution/210240.md"
+      },
+      {
+        "text": "Problem 210239",
+        "link": "/Solution/210239.md"
+      },
+      {
+        "text": "Problem 210238",
+        "link": "/Solution/210238.md"
+      },
+      {
+        "text": "Problem 210237",
+        "link": "/Solution/210237.md"
+      },
+      {
+        "text": "Problem 210081",
+        "link": "/Solution/210081.md"
+      },
+      {
+        "text": "Problem 210079",
+        "link": "/Solution/210079.md"
+      },
+      {
+        "text": "Problem 210055",
+        "link": "/Solution/210055.md"
+      },
+      {
+        "text": "Problem 210022",
+        "link": "/Solution/210022.md"
+      },
+      {
+        "text": "Problem 209998",
+        "link": "/Solution/209998.md"
+      },
+      {
+        "text": "Problem 209996",
+        "link": "/Solution/209996.md"
+      },
+      {
+        "text": "Problem 209995",
+        "link": "/Solution/209995.md"
+      },
+      {
+        "text": "Problem 209994",
+        "link": "/Solution/209994.md"
+      },
+      {
+        "text": "Problem 209993",
+        "link": "/Solution/209993.md"
+      },
+      {
+        "text": "Problem 209992",
+        "link": "/Solution/209992.md"
+      },
+      {
+        "text": "Problem 209991",
+        "link": "/Solution/209991.md"
+      },
+      {
+        "text": "Problem 209990",
+        "link": "/Solution/209990.md"
+      },
+      {
+        "text": "Problem 209989",
+        "link": "/Solution/209989.md"
+      },
+      {
+        "text": "Problem 209988",
+        "link": "/Solution/209988.md"
+      },
+      {
+        "text": "Problem 209978",
+        "link": "/Solution/209978.md"
+      },
+      {
+        "text": "Problem 209910",
+        "link": "/Solution/209910.md"
+      },
+      {
+        "text": "Problem 209908",
+        "link": "/Solution/209908.md"
+      },
+      {
+        "text": "Problem 209871",
+        "link": "/Solution/209871.md"
+      },
+      {
+        "text": "Problem 209863",
+        "link": "/Solution/209863.md"
+      },
+      {
+        "text": "Problem 209784",
+        "link": "/Solution/209784.md"
+      },
+      {
+        "text": "Problem 209782",
+        "link": "/Solution/209782.md"
+      },
+      {
+        "text": "Problem 209731",
+        "link": "/Solution/209731.md"
+      },
+      {
+        "text": "Problem 209730",
+        "link": "/Solution/209730.md"
+      },
+      {
+        "text": "Problem 209696",
+        "link": "/Solution/209696.md"
+      },
+      {
+        "text": "Problem 209649",
+        "link": "/Solution/209649.md"
+      }
+    ]
   }
 ];
