a84cc00bbb90054137c2b0a7cce717a68a37fcf9
CassiopeiaCode <i@5i.gs>
2025-07-18 12:52:29 +0800

    Automated deployment of frontend on 2025-07-18 12:52:29

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -341,6 +341,50 @@ export const sidebar = [
       {
         "text": "Problem 251496",
         "link": "/Solution/251496.md"
+      },
+      {
+        "text": "Problem 225893",
+        "link": "/Solution/225893.md"
+      },
+      {
+        "text": "Problem 225765",
+        "link": "/Solution/225765.md"
+      },
+      {
+        "text": "Problem 223734",
+        "link": "/Solution/223734.md"
+      },
+      {
+        "text": "Problem 210275",
+        "link": "/Solution/210275.md"
+      },
+      {
+        "text": "Problem 210274",
+        "link": "/Solution/210274.md"
+      },
+      {
+        "text": "Problem 210259",
+        "link": "/Solution/210259.md"
+      },
+      {
+        "text": "Problem 210256",
+        "link": "/Solution/210256.md"
+      },
+      {
+        "text": "Problem 210247",
+        "link": "/Solution/210247.md"
+      },
+      {
+        "text": "Problem 210246",
+        "link": "/Solution/210246.md"
+      },
+      {
+        "text": "Problem 210245",
+        "link": "/Solution/210245.md"
+      },
+      {
+        "text": "Problem 210244",
+        "link": "/Solution/210244.md"
       }
     ]
   }
